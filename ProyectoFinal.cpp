0d5c97c3f46334815fb4e07e6013e0647437963c
ANGEL CORREA <angel.zlcjz@gmail.com>
2024-05-09 10:24:20 -0600

    se eliminaron cosas del codigo

--- ProyectoFinal.cpp
+++ ProyectoFinal.cpp
@@ -236,7 +236,6 @@ int main()
 	mainWindow.Initialise();
 
 	CreateObjects();
-	CrearDado();
 	CreateShaders();
 
 	camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), -60.0f, 0.0f, 0.3f, 0.5f);
